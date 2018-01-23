9d172a9353eabcf2dff2c1c054e34cca555114ae
Ryan Dabler <rpdabler@gmail.com>
2018-01-23 11:09:53 -0700

    Hook up server to work with MongoDB

--- server.js
+++ server.js
@@ -1,31 +1,58 @@
-const express = require("express");
+"use strict";
+
+// Initialize
+const express  = require("express");
+const morgan   = require("morgan");
+const mongoose = require("mongoose");
+
 const app = express();
+
+const { routerCreator, routerWork } = require("./routers");
+
+const { PORT, DATABASE_URL } = require("./config");
 
 app.use(express.static("public"));
 
+// Log HTTP layer
+app.use(morgan("common"));
+
+// Set up router
+app.use("/api/creators", routerCreator);
+app.use("/api/works", routerWork);
+
+// Set up server
+mongoose.Promie = global.Promise;
+
 let server;
 
-function runServer() {
-  const port = process.env.PORT || 8080;
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err);
+    mongoose.connect(databaseUrl, {}, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
     });
   });
 }
 
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
   });
 }
