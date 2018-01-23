98394b82ad3042f7db88d97e57034c7a24f114d9
Ryan Dabler <rpdabler@gmail.com>
2018-01-23 11:08:43 -0700

    Set up creator model

---
+++ models.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+////////////////////////////
+// Set up creator data model
+////////////////////////////
+const creatorSchema = mongoose.Schema({
+  name:   {
+            first:  String,
+            middle: String,
+            last:  { type: String, required: true}
+          },
+  links:  [
+            {
+              domain: { type: String, required: true },
+              url:    { type: String, required: true }
+            }
+          ],
+  awards: [
+            {
+              award_name: { type: String, required: true },
+              award_year: { type: String, required: true }
+            }
+          ],
+  created: { type: String, default: Date.now }
+});
+
+creatorSchema.virtual("fullName")
+  .get(function() {
+    const nameFields = ["first", "middle", "last"];
+    let fullName = "";
+    
+    nameFields.forEach(nameField => {
+      if (this.name.hasOwnProperty(nameField) && this.name[nameField]) {
+        fullName += `${this.name[nameField]} `;
+      }
+    });
+    
+    return fullName.trim();
+  })
+  .set(function(fullName) {
+    const splitAuthor = fullName.split(" ");
+    
+    if (splitAuthor.length === 1) {
+      this.name.last = splitAuthor[0];
+    } else {
+      this.name.first = splitAuthor[0];
+      this.name.last  = splitAuthor[splitAuthor.length - 1];
+    
+      if (splitAuthor.length >= 3) {
+        this.name.middle = splitAuthor.slice(1, splitAuthor.length - 1).join(" ");
+      }
+    }
+  });
+  
+creatorSchema.methods.serialize = function() {
+  return {
+    id:      this._id,
+    name:    this.fullName,
+    links:   this.links,
+    awards:  this.awards,
+  };
+};
+
+const Creator = mongoose.model("Creator", creatorSchema);
+
+////////////////////////////
+// Set up work data model
+////////////////////////////
+
+
+////////////////////////////
+// Export models
+////////////////////////////
+module.exports = { Creator };
