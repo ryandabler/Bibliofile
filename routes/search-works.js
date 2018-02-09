36f1ca9100c574e60b04656423f7b7c934fa0a6d
Ryan Dabler <rpdabler@gmail.com>
2018-02-09 11:07:23 -0700

    Remove unnecessary require

--- routes/search-works.js
+++ routes/search-works.js
@@ -4,7 +4,6 @@ const express     = require("express");
 const router      = express.Router();
 const bodyParser  = require("body-parser");
 const jsonParser  = bodyParser.json();
-const mongoose    = require("mongoose");
 const { Work }    = require("../models");
 
 router.get("/:string", (req, res) => {
